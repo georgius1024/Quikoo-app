73408d91bee543565c4947911b1337b1ebeec835
Andrey-Yakovtsev <ayakovtsev@gmail.com>
2021-08-30 23:51:27 +0300

    Initial commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,119 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Quikoo App
+        </q-toolbar-title>
+
+        <div>Quikoo v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          Menu
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+
+const linksList = [
+  {
+    title: 'Profile',
+    caption: 'quasar.dev',
+    icon: 'record_voice_over',
+    link: '#'
+  },
+  {
+    title: 'Settings',
+    caption: 'github.com/quasarframework',
+    icon: 'settings',
+    link: '#'
+  },
+  {
+    title: 'Support',
+    caption: 'chat.quasar.dev',
+    icon: 'support',
+    link: '#'
+  },
+  {
+    title: 'Forum',
+    caption: 'forum.quasar.dev',
+    icon: 'record_voice_over',
+    link: '#'
+  },
+  {
+    title: 'My Quiks',
+    caption: '@quasarframework',
+    icon: 'rss_feed',
+    link: '#'
+  },
+  {
+    title: 'Facebook',
+    caption: '@QuasarFramework',
+    icon: 'public',
+    link: '#'
+  },
+  {
+    title: 'Quasar Awesome',
+    caption: 'Community Quasar projects',
+    icon: 'favorite',
+    link: '#'
+  }
+]
+
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'MainLayout',
+
+  components: {
+    EssentialLink
+  },
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      essentialLinks: linksList,
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+})
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,27 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quikoo background"
+      src="~assets/Images/Splashscreen/splashScreenBG.png"
+      style="width: 100%; height: auto"
+    >
+    <img class="z-top"
+      alt="Quikoo logo"
+      src="~assets/Images/Splashscreen/slashScreenLogo.png"
+      style="width: 150px; height: 150px"
+    >
+    <img class="z-top"
+      alt="Quikoo logo"
+      src="~assets/Images/Splashscreen/splashScreenBrand.png"
+      style="width: 100%; height: 100%"
+    >
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'PageIndex'
+})
+</script>
